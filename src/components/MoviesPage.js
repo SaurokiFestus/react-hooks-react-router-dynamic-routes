b79fd067f72d66dc10473cc3dd299a6972d6971a
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-07-31 13:15:30 +0300

    Done.

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+
+  const match = useRouteMatch();
+  console.log(match);
+
   return (
     <div>
+      
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list below</h3>
+      </Route>
+
       <MoviesList movies={movies} />
+
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
   );
 }
